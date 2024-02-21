66feb2c2a07570aa11b2ee2a65f0fcc54db4b5d2
au636396 <54847807+au636396@users.noreply.github.com>
2024-02-21 10:06:08 +0100

    Update streamlit_app.py trying ran num

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ import streamlit as st
 }
 
 # initializing with a random number
-if "condition" not in st.session_state:
-    st.session_state["condition"] = random.randint(1,4)
+if "rn" not in st.session_state:
+    st.session_state["rn"] = random.randint(1,100)
 
-st.write('st.session_state.condition') # this doesnt work and i don't know why- !!! investigate, code from here: https://discuss.streamlit.io/t/randomly-generate-a-number-and-save-this-number-and-user-input/20615/4
+st.write('st.session_state.rn') # this doesnt work and i don't know why- !!! investigate, code from here: https://discuss.streamlit.io/t/randomly-generate-a-number-and-save-this-number-and-user-input/20615/4
 
 
 #top text
