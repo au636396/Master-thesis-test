6456006056a41a5757f8d412a814c28d9d40aca0
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-05 11:47:25 +0100

    Update streamlit_app.py moved things around

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,17 +38,7 @@ st.write(st.session_state.condition)
 """
 st.image('basic_cookie.png')
 
-# Show different content based on the user session number.
-if st.session_state.condition == 1:
-    st.write("hello1")
-elif st.session_state.condition == 2:
-    st.write("hello2")
-elif st.session_state.condition == 3:
-    st.write("hello3")
-elif st.session_state.condition == 4:
-    st.write("hello4")
-else:
-    st.write("An error has occurred, please reload the page!")
+
 
 #defining the placment of the coloums (used for placing buttons) 
 col1, col2, col3, col4, col5 = st.columns([3,2,1,2,3])
@@ -61,19 +51,18 @@ with col4:
     st.write(afvis1)
 
 
-#trying to track the input of the user 
-#dfData = pd.read_csv("data.csv")
-#st.dataframe(df)
 
-#dfData = pd.concat([dfData, ])   # it cant do boll, soooooooo im trying the dumb ugly inefetions way
-
-
-# Create the pandas DataFrame
-df = pd.DataFrame(columns=['condition', 'button'])  # this works to make an empty dataframe
-df.to_csv('data.csv', index=False)  # i cant seems to save any thing, maybe looke here https://discuss.streamlit.io/t/adding-rows-in-st-data-editor-from-loaded-dataframe/46439
-
-st.dataframe(df)
-#df.append(afvis1)
+# Show different content based on the user session number.
+if st.session_state.condition == 1:
+    st.write("hello1")
+elif st.session_state.condition == 2:
+    st.write("hello2")
+elif st.session_state.condition == 3:
+    st.write("hello3")
+elif st.session_state.condition == 4:
+    st.write("hello4")
+else:
+    st.write("An error has occurred, please reload the page!")
 
 #show button with link to surevery
 st.link_button("Go to survey", "https://survey.au.dk/LinkCollector?key=VC8ZRNUQUN16")
