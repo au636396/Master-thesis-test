74e3d780aa6a24522577185ac5405eaf27d87790
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-08 12:32:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,7 @@ set_background('generic_website.png')
 
 css_body_container = f'''
 <style>
-    [data-testid="stSidebar"] + section [data-testid="stVerticalBlock"] div:nth-of-type({body})
-    [data-testid="stVerticalBlock"] {{background-color:rgba(175,238,238,.2)}}
+    {{background-color:rgba(175,238,238,.2)}}
 </style>
 '''
 st.markdown(css_body_container,unsafe_allow_html=True)
