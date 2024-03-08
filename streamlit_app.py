a8d3fb8c8d0e8e762385c3f800d66f5d35c8c99f
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-08 12:36:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ def set_background(png_file):
     </style>
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
-set_background('generic_website.png')
+#set_background('generic_website.png')
 
 css_body_container = f'''
 <style>
