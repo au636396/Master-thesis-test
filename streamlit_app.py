d1741d635a23d0d3ca6810669dc86ad3c77f60c0
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-11 11:56:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ col1, col2, col3 = st.columns([1,4,1])
 # making the cookie banner apir
 with col2:
     with st.container():
-        st.image('basic_cookie.png')
+        st.image('cookie_text.png')
         st_btn_group(buttons=buttons, key="5", gap_between_buttons = 45, size='default', align ='center')
 
 #.stVerticalBlock
