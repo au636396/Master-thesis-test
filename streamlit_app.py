3d6a48295ec9233fe9f37db18de31965efd15bd1
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-11 14:49:20 +0100

    Update streamlit_app.py removing old buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,16 +47,46 @@ def set_background(png_file):
 set_background('generic_website.png')
 
 
+#-------------------deifnng the way the buttons can look---------
+#con 1
 buttons1 = [
     { "label": "Accepter alle",
      "backgroundColor": "lightgreen", 
      "color": "black", 
-     "value": "accepter1",
-    },
+     "value": "accepter1",},
     {"label": " Afvis alle ", 
      "color": "black", 
-     "value": "afvis1",
-    },
+     "value": "afvis1",},
+]
+#con 2
+buttons2 = [
+    { "label": "Accepter alle",
+     "backgroundColor": "lightgreen", 
+     "color": "black", 
+     "value": "accepter2",},
+    {"label": " Afvis alle ", 
+     "color": "black", 
+     "value": "afvis2",},
+]
+#con 3
+buttons3 = [
+    {"label": " Afvis alle ", 
+     "color": "black", 
+     "value": "afvis3",},
+    { "label": "Accepter alle",
+     "backgroundColor": "lightgreen", 
+     "color": "black", 
+     "value": "accepter3",},
+]
+#con 4
+buttons4 = [
+    {"label": " Afvis alle ", 
+     "color": "black", 
+     "value": "afvis4",},
+    { "label": "Accepter alle",
+     "backgroundColor": "lightgreen", 
+     "color": "black", 
+     "value": "accepter4",},
 ]
 
 
@@ -81,12 +111,6 @@ with col2:
 if button_cliked:
     st.write(button_cliked)
 
-#.stVerticalBlock
-
-#.stHorizontalBlock
-#st-emotion-cache-115gedg e1f1d6gn3
-#st-emotion-cache-1wmy9hl e1f1d6gn1
-#stVerticalBlockBorderWrapper
 
 #defining the placment of the coloums (used for placing buttons). The st.coloum part defines how big each colum is so 2 is dubble as big as 1
 col1, col2, col4, col5 = st.columns([3,2,2,3])
@@ -101,38 +125,6 @@ col1, col2, col4, col5 = st.columns([3,2,2,3])
 if 'click' not in st.session_state:
     st.session_state['click'] = 'button not cliked'
 
-# Show different content based on the user session number.
-if st.session_state.condition == 1:  # con 1
-    with col2:
-        if st.button("Accepter alle", type="primary"):
-            st.session_state["click"] = "accepter1"
-    with col4:
-        if st.button("  Afvis alle  ", type="primary"):
-            st.session_state["click"] = "afvis1"
-elif st.session_state.condition == 2:  # con 2
-    with col2:
-        if st.button("Accepter alle", type="primary"):
-            st.session_state["click"] = "accepter2"
-    with col4:
-        if st.button("  Afvis alle  ", type="secondary"):
-            st.session_state["click"] = "afvis2"
-elif st.session_state.condition == 3:  # con 3
-    with col4:
-        if st.button("Accepter alle", type="primary"):
-            st.session_state["click"] = "accepter3"
-    with col2:
-        if st.button("  Afvis alle  ", type="primary"):
-            st.session_state["click"] = "afvis3"
-elif st.session_state.condition == 4:  # con 4
-    with col4:
-        if st.button("Accepter alle", type="primary"):
-            st.session_state["click"] = "accepter4"
-    with col2:
-        if st.button("  Afvis alle  ", type="secondary"):
-            st.session_state["click"] = "afvis4"
-else:
-    st.write("An error has occurred, please reload the page!")
-    
 
 ## take the button input and puts it in the new row dataframe, only after a buttons has been pressed
 if st.session_state.click != 'button not cliked':
@@ -150,4 +142,38 @@ if st.session_state.click != 'button not cliked':
 
 
 
+
+###-------------------- old code for showinr the numberes------ 
+# Show different content based on the user session number.
+#if st.session_state.condition == 1:  # con 1
+#    with col2:
+#        if st.button("Accepter alle", type="primary"):
+#            st.session_state["click"] = "accepter1"
+#    with col4:
+#        if st.button("  Afvis alle  ", type="primary"):
+#            st.session_state["click"] = "afvis1"
+#elif st.session_state.condition == 2:  # con 2
+#    with col2:
+#        if st.button("Accepter alle", type="primary"):
+#            st.session_state["click"] = "accepter2"
+#    with col4:
+#        if st.button("  Afvis alle  ", type="secondary"):
+#            st.session_state["click"] = "afvis2"
+#elif st.session_state.condition == 3:  # con 3
+#    with col4:
+#        if st.button("Accepter alle", type="primary"):
+#            st.session_state["click"] = "accepter3"
+#    with col2:
+#        if st.button("  Afvis alle  ", type="primary"):
+#            st.session_state["click"] = "afvis3"
+#elif st.session_state.condition == 4:  # con 4
+#    with col4:
+#        if st.button("Accepter alle", type="primary"):
+#            st.session_state["click"] = "accepter4"
+#    with col2:
+#        if st.button("  Afvis alle  ", type="secondary"):
+#            st.session_state["click"] = "afvis4"
+#else:
+#    st.write("An error has occurred, please reload the page!")
+    
 
