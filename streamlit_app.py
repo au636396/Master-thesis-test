414ed6fdd16a5b95b530364915d84c1ffe9fb2ad
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-11 15:59:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,55 +137,12 @@ else:
 if st.session_state.click != 'button not cliked':
     new_row = pd.DataFrame([[st.session_state.click]], columns=['button']) 
     st.dataframe(new_row)
-#st.dataframe(new_row)   #!!! remove this before experiment launch
-#newdata = pd.concat([pdolddata, new_row])    # adding the new row from above at the end of the data
-#gd.set_with_dataframe(olddata, newdata)    #this should ad the new data to the gsheet
+st.dataframe(new_row)   #!!! remove this before experiment launch
+newdata = pd.concat([pdolddata, new_row])    # adding the new row from above at the end of the data
+gd.set_with_dataframe(olddata, newdata)    #this should ad the new data to the gsheet
 
 
 
 #show button with link to surevery only after a button has been cliked 
 if st.session_state.click != 'button not cliked':
     st.link_button("Go to survey", "https://survey.au.dk/LinkCollector?key=VC8ZRNUQUN16")
-
-
-
-
-###-------------------- old code for showinr the numberes--------------------------------------------------- 
-
-
-#defining the placment of the coloums (used for placing buttons). The st.coloum part defines how big each colum is so 2 is dubble as big as 1
-#col1, col2, col4, col5 = st.columns([3,2,2,3])
-
-# Show different content based on the user session number.
-#if st.session_state.condition == 1:  # con 1
-#    with col2:
-#        if st.button("Accepter alle", type="primary"):
-#            st.session_state["click"] = "accepter1"
-#    with col4:
-#        if st.button("  Afvis alle  ", type="primary"):
-#            st.session_state["click"] = "afvis1"
-#elif st.session_state.condition == 2:  # con 2
-#    with col2:
-#        if st.button("Accepter alle", type="primary"):
-#            st.session_state["click"] = "accepter2"
-#    with col4:
-#        if st.button("  Afvis alle  ", type="secondary"):
-#            st.session_state["click"] = "afvis2"
-#elif st.session_state.condition == 3:  # con 3
-#    with col4:
-#        if st.button("Accepter alle", type="primary"):
-#            st.session_state["click"] = "accepter3"
-#    with col2:
-#        if st.button("  Afvis alle  ", type="primary"):
-#            st.session_state["click"] = "afvis3"
-#elif st.session_state.condition == 4:  # con 4
-#    with col4:
-#        if st.button("Accepter alle", type="primary"):
-#            st.session_state["click"] = "accepter4"
-#    with col2:
-#        if st.button("  Afvis alle  ", type="secondary"):
-#            st.session_state["click"] = "afvis4"
-#else:
-#    st.write("An error has occurred, please reload the page!")
-    
-
