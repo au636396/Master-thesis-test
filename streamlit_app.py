479e24c6d8e225282746bcea3cb737396e1930d3
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-12 15:13:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,8 @@ if button_cliked == 'afvis1' or 'accepter1' or 'afvis2' or 'accepter2' or 'afvis
                     st.session_state['click'] = button_cliked
 
 t_of_button_cliked = type(button_cliked)
-st.write(t_of_button_cliked)
+a = str(t_of_button_cliked)
+st.write(a)
 st.write(st.session_state['click']) #for tesing can be removed 
 
 #--------------- traking the button click ---------------
