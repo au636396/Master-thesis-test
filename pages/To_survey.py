ad39bd69a6b23e8ad74737ae3cf792504aadc18f
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-15 11:20:06 +0100

    Update To_survey.py

--- pages/To_survey.py
+++ pages/To_survey.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
+#removing the side bar, header, and footer
+st.set_page_config(initial_sidebar_state="collapsed")
+
+css='''
+<style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+'''
+st.markdown(css, unsafe_allow_html=True)
+
 st.text("hello me")
