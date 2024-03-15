69e4f6cdbd6fecb47b1ea777ec9201476241651e
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-15 14:58:32 +0100

    Update streamlit_app.py shutting off the tracking for now

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,8 @@ set_background('generic_website.png')
 
 #----------------------------------------- SETUP FOR SAVING DATA  --------------------------------------------------------
 gc = gspread.service_account(filename='~/.config/gspread/service_account.json')   #cornnects to API
-olddata = gc.open("MasterThesisDataLog").worksheet("ark") # spesifies the sheet
-pdolddata = gd.get_as_dataframe(olddata)  #imports it as a pd dataframe 
+#olddata = gc.open("MasterThesisDataLog").worksheet("ark") # spesifies the sheet
+#pdolddata = gd.get_as_dataframe(olddata)  #imports it as a pd dataframe 
 
 #------------------------- DEFINING THE WAY THE BUTTONS LOOK AND THE OUTPUT TEXT THEY GIVE --------------------------------
 #con1
@@ -129,10 +129,10 @@ else:
 
 #---------------------------------------------- TRACKING THE BUTTON INPUT -----------------------------------------------------------
 ## take the button input and puts it in the new row dataframe, only after a buttons has been pressed
-if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked == 'afvis2' or button_cliked == 'accepter2' or button_cliked == 'afvis3' or button_cliked == 'accepter3' or button_cliked == 'afvis4' or button_cliked == 'accepter4':
-        new_row = pd.DataFrame([button_cliked], columns=['button'])   #making the new dataframe
-        newdata = pd.concat([pdolddata, new_row]) # adding the new row from above at the end of the data
-        gd.set_with_dataframe(olddata, newdata)  #this should ad the new data to the gsheet
+#if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked == 'afvis2' or button_cliked == 'accepter2' or button_cliked == 'afvis3' or button_cliked == 'accepter3' or button_cliked == 'afvis4' or button_cliked == 'accepter4':
+#        new_row = pd.DataFrame([button_cliked], columns=['button'])   #making the new dataframe
+#        newdata = pd.concat([pdolddata, new_row]) # adding the new row from above at the end of the data
+#        gd.set_with_dataframe(olddata, newdata)  #this should ad the new data to the gsheet
 
 #----------------- SWITH TO NEW PAGE-------------------------------------
 #Goes to survey page once its has been cliked 
