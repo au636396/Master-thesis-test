0d137f4a7da80c8b855253f73e74874cf1cc265d
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-15 14:44:45 +0100

    Update To_survey.py

--- pages/To_survey.py
+++ pages/To_survey.py
@@ -19,6 +19,8 @@ st.markdown(css, unsafe_allow_html=True)
 st.subheader('Tak for at du ville svare på mit spørgeskema.')
 st.subheader("Click på knappen under for at gå til spørgeskemaet.")
 
-st.write("#"Click på knappen under for at gå til spørgeskemaet."")
+text2 = "Click på knappen under for at gå til spørgeskemaet."
+
+St.write(“# "+text2)
 
 st.link_button("Til spørgeskema", "https://survey.au.dk/LinkCollector?key=VC8ZRNUQUN16")
