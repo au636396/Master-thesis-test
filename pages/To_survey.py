4e15605b86501d8931029217ac2c2c5301d612ff
au636396 <54847807+au636396@users.noreply.github.com>
2024-03-15 14:52:45 +0100

    Update To_survey.py

--- pages/To_survey.py
+++ pages/To_survey.py
@@ -15,8 +15,10 @@ css='''
 '''
 st.markdown(css, unsafe_allow_html=True)
 
+col1, col2, col3 = st.columns([2,1,2])
 #The visuals
 st.write("##### "+"Tak for at du ville svare på mit spørgeskema.")
 st.write("##### "+"Click på knappen under for at gå til spørgeskemaet.")
-
-st.link_button("**Til spørgeskema**", "https://survey.au.dk/LinkCollector?key=VC8ZRNUQUN16")
+st.write()
+with col2:
+    st.link_button("**Til spørgeskema**", "https://survey.au.dk/LinkCollector?key=VC8ZRNUQUN16")
