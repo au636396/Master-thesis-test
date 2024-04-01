2220ed8cb81a291f855c2f2413fd67ed7a474e94
au636396 <54847807+au636396@users.noreply.github.com>
2024-04-01 12:58:46 +0200

    Update streamlit_app.py trunign on trakcng for text

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,10 +123,10 @@ with col2:
     st.markdown('##')
 #---------------------------------------------- TRACKING THE BUTTON INPUT -----------------------------------------------------------
 ## take the button input and puts it in the new row dataframe, only after a buttons has been pressed
-#if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked == 'afvis2' or button_cliked == 'accepter2' or button_cliked == 'afvis3' or button_cliked == 'accepter3' or button_cliked == 'afvis4' or button_cliked == 'accepter4':
-#        new_row = pd.DataFrame([button_cliked], columns=['button'])   #making the new dataframe
-#        newdata = pd.concat([pdolddata, new_row]) # adding the new row from above at the end of the data
-#        gd.set_with_dataframe(olddata, newdata)  #this should ad the new data to the gsheet
+if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked == 'afvis2' or button_cliked == 'accepter2' or button_cliked == 'afvis3' or button_cliked == 'accepter3' or button_cliked == 'afvis4' or button_cliked == 'accepter4':
+        new_row = pd.DataFrame([button_cliked], columns=['button'])   #making the new dataframe
+        newdata = pd.concat([pdolddata, new_row]) # adding the new row from above at the end of the data
+        gd.set_with_dataframe(olddata, newdata)  #this should ad the new data to the gsheet
 
 #----------------- SWITH TO NEW PAGE-------------------------------------
 #Goes to survey page once its has been cliked 
