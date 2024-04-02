c7e204662af21f836a5439ed937cc482a2f6f3da
au636396 <54847807+au636396@users.noreply.github.com>
2024-04-02 13:26:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,16 +133,15 @@ if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked ==
 #---------------------------------------------- TRACKING THE BUTTON INPUT -----------------------------------------------------------
 ## take the button input and puts it in the new row dataframe, only after a buttons has been pressed
 if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked == 'afvis2' or button_cliked == 'accepter2' or button_cliked == 'afvis3' or button_cliked == 'accepter3' or button_cliked == 'afvis4' or button_cliked == 'accepter4':
-        new_row = pd.DataFrame([button_cliked], columns = ['button'])   #making the new dataframe
-        new_row['timer'] = elapsed_time   #adding time to the new dataframe
-        newdata = pd.concat([pdolddata, new_row]) # adding the new row from above at the end of the data
-        gd.set_with_dataframe(olddata, newdata)  #this should ad the new data to the gsheet
+    new_row = pd.DataFrame([button_cliked], columns = ['button'])   #making the new dataframe
+    new_row['timer'] = elapsed_time   #adding time to the new dataframe
+    newdata = pd.concat([pdolddata, new_row]) # adding the new row from above at the end of the data
+    gd.set_with_dataframe(olddata, newdata)  #this should ad the new data to the gsheet
 
 #-------------------------------------------------- SWITH TO NEW PAGE-----------------------------------------------------------------
 #Goes to survey page once its has been cliked 
-with col2:
-   if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked == 'afvis2' or button_cliked == 'accepter2' or button_cliked == 'afvis3' or button_cliked == 'accepter3' or button_cliked == 'afvis4' or button_cliked == 'accepter4':
-          st.switch_page("pages/To_survey.py")
+if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked == 'afvis2' or button_cliked == 'accepter2' or button_cliked == 'afvis3' or button_cliked == 'accepter3' or button_cliked == 'afvis4' or button_cliked == 'accepter4':
+    st.switch_page("pages/To_survey.py")
 
 
 #--------------------------------------------ADD IF YOU WANT MORE CONDITIONS-----------------------------------------------------------
