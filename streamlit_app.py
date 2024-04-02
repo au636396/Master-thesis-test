2326bf43343dfc5a6a232892aa8bfabe1adf9ece
au636396 <54847807+au636396@users.noreply.github.com>
2024-04-02 12:49:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,14 +125,12 @@ else:
 
 with col2: 
     st.markdown('##')
-
-
-
-#---------------------------------------------- TIMER END -----------------------------------------------------------
+#---------------------------------------------------------- TIMER END --------------------------------------------------------------
 if button_cliked == 'afvis1' or button_cliked == 'accepter1' or button_cliked == 'afvis2' or button_cliked == 'accepter2' or button_cliked == 'afvis3' or button_cliked == 'accepter3' or button_cliked == 'afvis4' or button_cliked == 'accepter4':
     end_time = time.time()
     elapsed_time = end_time - start_time
     st.write(elapsed_time)
+    st.write(type(elapsed_time))
 #---------------------------------------------- TRACKING THE BUTTON INPUT -----------------------------------------------------------
 ## take the button input and puts it in the new row dataframe, only after a buttons has been pressed
 st.dataframe(pdolddata)
