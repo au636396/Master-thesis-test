2ea971f8b94539c1dc78332a8b73cc3195fd7837
au636396 <54847807+au636396@users.noreply.github.com>
2024-04-05 11:56:40 +0200

    Update To_survey.py slightly chsnging text

--- pages/To_survey.py
+++ pages/To_survey.py
@@ -18,7 +18,7 @@ st.markdown(css, unsafe_allow_html=True)
 #The visuals
 col1, col2, col3 = st.columns([1,3,1])
 with col2:
-    st.write("Tak for at du ville hjælpe med mit speciale. Jeg undersøger designet af cookie pop-ups på hjemmesider, og hvordan de påvirker adfærd.")
-    st.write("Jeg vil gerne stille dig nogle spørgsmål omkring cookie pop-ups og databeskyttelse. Det ville tage ca. 5 minuter. Hvis du gerne vil hjælpe mig med det, så klik her:")
+    st.write("Tak for at du ville hjælpe med mit speciale. Jeg undersøger designet af hjemmesider, mere specifikt af designet cookie pop-ups, og hvordan de påvirker adfærd.")
+    st.write("Jeg vil gerne stille dig nogle spørgsmål omkring cookie pop-ups og databeskyttelse. Det ville tage ca. 2-5 minutter. Hvis du gerne vil hjælpe mig med det, så klik her:")
     st.write()
     st.link_button("**Til spørgeskema**", "https://survey.au.dk/LinkCollector?key=VC8ZRNUQUN16", type="primary")
